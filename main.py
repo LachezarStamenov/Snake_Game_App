b410f1a6a4c9b62de1f55364514132a2d1f88232
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-04-12 09:03:35 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+start_position = [(0, 0), (-20, 0), (-40, 0)]
